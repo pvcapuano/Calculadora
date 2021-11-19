74f23d8717b4b9065483acf039f8dff69b51503d
pvcapuano <pvcapuano@gmail.com>
2021-11-19 15:56:32 -0200

    add html structure and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import { 
+  Card, 
+  Container, 
+  Row, 
+  Col,
+  Button,
+  Form 
+} from "react-bootstrap"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Calculadora</h1>
+    <Card style={{
+      background: 'tranparente !important',
+      backgroundColor: "#242730",
+      padding: "10px",
+      width: "400px",
+      borderRadius: "0px"
+    }}>
+    <Container>
+      <Row>
+          <Col>
+          <Form.Control type="text"
+          name="txtNumeros"
+          class="text-right"
+          readOnly="readonly" />
+          </Col>
+      </Row>
+
+      <Row>
+        <Col xs="6">
+          <Button variant="secondary">C</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">%</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">/</Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs="3">
+          <Button variant="secondary">7</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">8</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">9</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">x</Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs="3">
+          <Button variant="secondary">4</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">5</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">6</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">-</Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs="3">
+          <Button variant="secondary">1</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">2</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">3</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">+</Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs="6">
+          <Button variant="secondary">0</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">,</Button>
+        </Col>
+        <Col xs="3">
+          <Button variant="secondary">=</Button>
+        </Col>
+      </Row>
+
+    </Container>
+      
+    </Card>
+    </>
   );
 }
 
